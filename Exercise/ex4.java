4d8f8d3305e988d4d58f15bf95bb94ee78c5b87a
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2025-03-17 12:08:15 +0000

    Added Java program to print odd numbers between 1 and 20

---
+++ Exercise/ex4.java
@@ -0,0 +1,11 @@
+//Print the odd numbers between 1 to 20
+
+public class ex4 {
+    public static void main(String args[]) {
+        for(int i=1; i<=20; i++) {
+            if(i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
