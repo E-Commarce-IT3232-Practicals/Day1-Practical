9809481f8662882572574e4c210e2c6f900f33f8
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2025-03-17 12:49:12 +0000

    Edit

--- Exercise/Student.java
+++ Exercise/Student.java
@@ -1,3 +1,7 @@
+//Create a student class  
+//create 5 student objects and store it in  a proper datastructure 
+//find the average marks of the students 
+
 import java.util.ArrayList;
 
 class Student {
